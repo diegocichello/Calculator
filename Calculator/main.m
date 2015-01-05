5d678b273423213d50bf2de11179c4399ed935fc
Diego Cichello <diegocichello@Diegos-MacBook-Pro.local>
2015-01-05 14:52:29 -0800

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Diego Cichello on 1/5/15.
+//  Copyright (c) 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
